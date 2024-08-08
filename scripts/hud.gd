b3d4d0f59b425b5dfa08c0f22c2d530486b70995
JaspherCustodio <27jasphercustodio@gmail.com>
2024-08-08 22:35:19 +0800

    buggy

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -8,10 +8,13 @@ class_name UI
 @onready var SFX_BUS_ID = AudioServer.get_bus_index("SFX")
 @onready var MUSIC_BUS_ID = AudioServer.get_bus_index("Music")
 @onready var settings_menu = $SettingsMenu
-@onready var settings_button = $GameReadyMenu/SettingsButton
+@onready var settings_button_game_ready = $GameReadyMenu/SettingsButton
 @onready var pause_menu = $PauseMenu
 @onready var pause_button = $PauseButton
+@onready var game_over_menu = $GameOverMenu
 @onready var animation_player = $AnimationPlayer
+@onready var music_slider = $SettingsMenu/MarginContainer/VBoxContainer/GridContainer/MusicSlider
+
 
 func _ready():
 	animation_player.play("RESET")
@@ -25,6 +28,7 @@ func on_game_over(score, high_score):
 	pause_button.hide()
 	get_tree().paused = true
 	animation_player.play("blur")
+	animation_player.play("game_over_fade")
 	scored_label.text = "SCORE: " + format_number_with_commas(score) + "M"
 	high_score_label.text = "HIGH SCORE: " + format_number_with_commas(high_score) + "M"
 	$GameOverMenu/GameOver/HBoxContainer/Restart.grab_focus()
@@ -45,14 +49,16 @@ func format_number_with_commas(number: int) -> String:
 func _on_pause_pressed() -> void:
 	get_tree().paused = true
 	pause_button.hide()
-	animation_player.play("blur")
 	pause_menu.show()
+	animation_player.play("blur")
+	animation_player.play("pause_fade")
 
 func _on_resume_pressed() -> void:
 	pause_button.show()
 	pause_menu.hide()
 	get_tree().paused = false
 	animation_player.play_backwards("blur")
+	animation_player.play_backwards("pause_fade")
 
 func _on_restart_pressed() -> void:
 	_on_resume_pressed()
@@ -69,21 +75,18 @@ func _on_sfx_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(SFX_BUS_ID, linear_to_db(value))
 	AudioServer.set_bus_mute(SFX_BUS_ID, value < 0.5)
 
-func _on_settings_button_pressed():
-	if settings_button.pressed:
-		settings_menu.show()
-		settings_button.hide()
-		$SettingsMenu/MarginContainer/VBoxContainer/GridContainer/MusicSlider.grab_focus()
-
 func _on_pause_button_pressed():
 	if pause_button.pressed and !get_tree().paused:
 		_on_pause_pressed()
 	elif pause_button.pressed and get_tree().paused:
 		_on_resume_pressed()
 
+func _on_settings_button_pressed():
+	settings_menu.show()
+	animation_player.play("settings_fade")
+	music_slider.grab_focus()
+
 func _on_back_button_pressed():
 	settings_menu.hide()
-	settings_button.show()
+	animation_player.play_backwards("settings_fade")
 
-func _process(delta):
-	_on_back_button_pressed()
